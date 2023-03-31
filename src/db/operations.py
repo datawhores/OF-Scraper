c9cd464c8685c877e478adc47777745fdd4d7429
excludedBittern8 <67020411+excludedBittern8@users.noreply.github.com>
2023-03-31 16:56:38 -0500

    create db and caching for posts and messaging

--- src/db/operations.py
+++ src/db/operations.py
@@ -12,31 +12,82 @@ import contextlib
 import glob
 import pathlib
 import sqlite3
+import json
 from itertools import chain
 from hashlib import md5
 from rich.console import Console
 console=Console()
-from ..constants import configPath, databaseFile
+from ..constants import configPath
 from ..utils import separate, profiles
+from ..db import queries
+from ..utils.paths import createDir,databasePathHelper,messageResponsePathHelper,timelineResponsePathHelper
+
+	# paid INTEGER, 
+	# archived BOOLEAN, 
 
 
-def create_database(model_id, path=None):
-    profile = profiles.get_current_profile()
-
-    path = path or pathlib.Path.home() / configPath / profile / databaseFile
-
-    with contextlib.closing(sqlite3.connect(path,check_same_thread=False)) as conn:
+def write_messages(data: dict, model_id,username,path=None):
+    datebase_path =databasePathHelper(path,model_id,username)
+    createDir(datebase_path.parent)
+    with contextlib.closing(sqlite3.connect(datebase_path,check_same_thread=False)) as conn:
         with contextlib.closing(conn.cursor()) as cur:
-            try:
-                model_sql = f"""
-                CREATE TABLE '{model_id}'(
-                    id INTEGER PRIMARY KEY,
-                    media_id INTEGER NOT NULL,
-                    filename VARCHAR NOT NULL
-                );"""
-                cur.execute(model_sql)
-            except sqlite3.OperationalError:
-                pass
+            if len(cur.execute(queries.messageDupeCheck,(data['id'],)).fetchall())==0:
+                insertData=(data["id"],data["text"],data.get('price') or 0,len(list(filter(lambda x:x['canView']==False,data['media'])))==0,0,data["createdAt"],model_id)
+                cur.execute(queries.messagesInsert,insertData)
+                conn.commit()
+
+def save_messages_response(model_id,username,messages,path=None):
+    messagepath =messageResponsePathHelper(path,model_id,username)
+    createDir(messagepath.parent)
+    with open(messagepath,"w") as p:
+        p.write(json.dumps({"posts":messages}))
+
+def read_messages_response(model_id,username,path=None):
+    messagepath =messageResponsePathHelper(path,model_id,username)
+    if pathlib.Path(messagepath).exists():
+        with open(messagepath,"r") as p:
+            messages=json.loads(p.read() or '{"posts": []}')["posts"]
+        if len(messages)>0:
+            return messages
+    return []
+            
+  
+ 
+
+def write_post(data: dict, model_id,username,path=None):
+    datebase_path =databasePathHelper(path,model_id,username)
+    createDir(datebase_path.parent)
+    with contextlib.closing(sqlite3.connect(datebase_path,check_same_thread=False)) as conn:
+        with contextlib.closing(conn.cursor()) as cur:
+            if len(cur.execute(queries.postDupeCheck,(data['id'],)).fetchall())==0:
+                insertData=(data["id"],data["text"],data.get('price') or 0,data['isOpen'],0,data["postedAt"])
+                cur.execute(queries.postInsert,insertData)
+                conn.commit()
+
+
+
+def save_timeline_response(model_id,username,posts,path=None):
+    messagepath =timelineResponsePathHelper(path,model_id,username)
+    createDir(messagepath.parent)
+    with open(messagepath,"w") as p:	
+        p.write(json.dumps({"posts":posts}))
+
+def read_timeline_response(model_id,username,path=None):
+    messagepath =timelineResponsePathHelper(path,model_id,username)
+    if pathlib.Path(messagepath).exists():
+        with open(messagepath,"r") as p:
+            messages=json.loads(p.read() or '{"posts": []}')["posts"]
+        if len(messages)>0:
+            return messages
+    return []
+            
+
+
+
+
+
+
+
 
 
 def write_from_data(data: tuple, model_id):
@@ -53,7 +104,12 @@ def write_from_data(data: tuple, model_id):
             VALUES (?, ?);"""
             cur.execute(model_insert_sql, data)
             conn.commit()
-
+def insert_combined_urls(combined_urls,model_id,usernames,path=None):
+    for dict in combined_urls:
+        if dict.get("responsetype")=="messages":
+            write_messages(dict,model_id,usernames,path=None)
+        elif dict.get("responsetype")=="post":
+            write_post(dict,model_id,usernames,path=None)
 
 def read_foreign_database(path) -> list:
     database_files = glob.glob(path.strip('\'\"') + '/*.db')
