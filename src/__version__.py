0ca29dd45e915ee2b62f0a1ab1948fdd8aaf574f
excludedBittern8 <67020411+excludedBittern8@users.noreply.github.com>
2023-04-07 20:42:14 -0500

    improve textsplit

--- src/__version__.py
+++ src/__version__.py
@@ -8,7 +8,7 @@ r"""
 """
 
 __title__ = 'ofscraper'
-__version__ = '1.65'
+__version__ = '1.71'
 __author__ = 'excludedBittern8'
 __author_email__ = 'excludedBittern8@riseup.net'
 __description__ = 'A command-line program to quickly download,like or unlike posts, and more'
