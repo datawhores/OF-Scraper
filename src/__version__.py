9f18921beb76fe187ab2ffb6c10999264271ef7e
excludedBittern8 <67020411+excludedBittern8@users.noreply.github.com>
2023-04-09 11:35:12 -0500

    update to 1.75

--- src/__version__.py
+++ src/__version__.py
@@ -8,7 +8,7 @@ r"""
 """
 
 __title__ = 'ofscraper'
-__version__ = '1.74'
+__version__ = '1.75'
 __author__ = 'excludedBittern8'
 __author_email__ = 'excludedBittern8@riseup.net'
 __description__ = 'A command-line program to quickly download,like or unlike posts, and more'
