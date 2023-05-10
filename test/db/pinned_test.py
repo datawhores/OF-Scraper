64fda087a39cedc60873c7832891038ebd8bc175
datawhores <67020411+datawhores@users.noreply.github.com>
2023-05-09 22:20:59 -0500

    remove deprechaed test

--- test/db/pinned_test.py
+++ test/db/pinned_test.py
@@ -37,33 +37,3 @@ def test_pinned_insert_failure(mocker):
             create_post_table("11111","test")
             write_post_table(Post(PINNED_POSTS_EXAMPLE,"111","test2"))
 
-def test_pinned_response(mocker):
-    with tempfile.NamedTemporaryFile() as p:
-        try:
-            mocker.patch("src.utils.paths.timelineResponsePathHelper",return_value=p.name)
-            save_pinned_response(PINNED_POSTS_EXAMPLE,"11111","test")
-        except Exception as E:
-            print(E)
-            raise Exception
-def test_pinned_response_failure(mocker):
-    with tempfile.NamedTemporaryFile() as p:   
-        with pytest.raises(Exception):
-            mocker.patch("src.utils.paths.timelineResponsePathHelper",return_value=p.name)
-            save_pinned_response("11111",None)
-
-
-def test_read_pinned_response(mocker):
-    with tempfile.NamedTemporaryFile() as p:
-        try:
-            mocker.patch("src.utils.paths.timelineResponsePathHelper",return_value=p.name)
-            save_pinned_response(PINNED_POSTS_EXAMPLE,"11111","test")
-            read_pinned_response("11111","test")
-        except Exception as E:
-            print(E)
-            raise Exception
-def test_read_pinned_response_failure(mocker):
-    with tempfile.NamedTemporaryFile() as p:   
-        with pytest.raises(Exception):
-            mocker.patch("src.utils.paths.postResponsePathHelpe",return_value=p.name)
-            save_pinned_response(PINNED_POSTS_EXAMPLE,"11111","test")
-            read_pinned_response("11112","test")
--- test/db/post_test.py
+++ test/db/post_test.py
@@ -36,34 +36,3 @@ def test_post_insert_failure(mocker):
             mocker.patch("src.utils.paths.databasePathHelper",return_value=p.name)
             create_post_table("11111","test")
             write_post_table(Post(TIMELINE_EXAMPLE,"111","test2"))
-
-def test_post_response(mocker):
-    with tempfile.NamedTemporaryFile() as p:
-        try:
-            mocker.patch("src.utils.paths.timelineResponsePathHelper",return_value=p.name)
-            save_timeline_response(TIMELINE_EXAMPLE,"11111","test")
-        except Exception as E:
-            print(E)
-            raise Exception
-def test_post_response_failure(mocker):
-    with tempfile.NamedTemporaryFile() as p:   
-        with pytest.raises(Exception):
-            mocker.patch("src.utils.paths.timelineResponsePathHelper",return_value=p.name)
-            save_timeline_response("11111",None)
-
-
-def test_read_post_response(mocker):
-    with tempfile.NamedTemporaryFile() as p:
-        try:
-            mocker.patch("src.utils.paths.timelineResponsePathHelper",return_value=p.name)
-            save_timeline_response(TIMELINE_EXAMPLE,"11111","test")
-            read_timeline_response("11111","test")
-        except Exception as E:
-            print(E)
-            raise Exception
-def test_read_post_response_failure(mocker):
-    with tempfile.NamedTemporaryFile() as p:   
-        with pytest.raises(Exception):
-            mocker.patch("src.utils.paths.postResponsePathHelpe",return_value=p.name)
-            save_timeline_response(TIMELINE_EXAMPLE,"11111","test")
-            read_timeline_response("11112","test")
