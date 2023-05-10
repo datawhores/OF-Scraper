c63139eeadc0031d45926a395567a10c051fabc8
datawhores <67020411+datawhores@users.noreply.github.com>
2023-05-09 22:20:59 -0500

    remove deprechaed test

--- test/db/archive_test.py
+++ test/db/archive_test.py
@@ -37,33 +37,3 @@ def test_archive_insert_failure(mocker):
             create_post_table("11111","test")
             write_post_table(Post(ARCHIVED_POST_EXAMPLE,"111","test2"))
 
-def test_archive_response(mocker):
-    with tempfile.NamedTemporaryFile() as p:
-        try:
-            mocker.patch("src.utils.paths.timelineResponsePathHelper",return_value=p.name)
-            save_archive_response(ARCHIVED_POST_EXAMPLE,"11111","test")
-        except Exception as E:
-            print(E)
-            raise Exception
-def test_archive_response_failure(mocker):
-    with tempfile.NamedTemporaryFile() as p:   
-        with pytest.raises(Exception):
-            mocker.patch("src.utils.paths.timelineResponsePathHelper",return_value=p.name)
-            save_archive_response("11111",None)
-
-
-def test_read_archive_response(mocker):
-    with tempfile.NamedTemporaryFile() as p:
-        try:
-            mocker.patch("src.utils.paths.timelineResponsePathHelper",return_value=p.name)
-            save_archive_response(ARCHIVED_POST_EXAMPLE,"11111","test")
-            read_archive_response("11111","test")
-        except Exception as E:
-            print(E)
-            raise Exception
-def test_read_archive_response_failure(mocker):
-    with tempfile.NamedTemporaryFile() as p:   
-        with pytest.raises(Exception):
-            mocker.patch("src.utils.paths.postResponsePathHelpe",return_value=p.name)
-            save_archive_response(ARCHIVED_POST_EXAMPLE,"11111","test")
-            read_archive_response("11112","test")
