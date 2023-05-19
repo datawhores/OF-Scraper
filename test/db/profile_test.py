6867eb0106ef8e2ef872995e7c9ec5d2db09b7ec
datawhores <67020411+datawhores@users.noreply.github.com>
2023-05-19 13:07:38 -0500

    fix for tests
    Former-commit-id: 05f4c3ccd7fff4817e4590d508719978acbcb88d

--- test/db/profile_test.py
+++ test/db/profile_test.py
@@ -7,7 +7,7 @@ from ofscraper.api.posts import Post,Media
 def test_profile_create(mocker):
     with tempfile.NamedTemporaryFile() as p:
         try:
-            mocker.patch("src.utils.paths.databasePathHelper",return_value=p.name)
+            mocker.patch("ofscraper.utils.paths.databasePathHelper",return_value=p.name)
             create_profile_table("11111","test")
         except:
             raise Exception
@@ -17,14 +17,14 @@ def test_profile_create(mocker):
 def test_profile_failure(mocker):
     with tempfile.NamedTemporaryFile() as p:   
         with pytest.raises(Exception):
-            mocker.patch("src.utils.paths.databasePathHelper",return_value=p.name)
+            mocker.patch("ofscraper.utils.paths.databasePathHelper",return_value=p.name)
             create_profile_table("11111")
 
 
 def test_profile_insert(mocker):
     with tempfile.NamedTemporaryFile() as p:
         try:
-            mocker.patch("src.utils.paths.databasePathHelper",return_value=p.name)
+            mocker.patch("ofscraper.utils.paths.databasePathHelper",return_value=p.name)
             create_profile_table("11111","test")
             write_profile_table("11111","test")
         except Exception as E:
@@ -33,6 +33,6 @@ def test_profile_insert(mocker):
 def test_profile_insert_failure(mocker):
     with tempfile.NamedTemporaryFile() as p:   
         with pytest.raises(Exception):
-            mocker.patch("src.utils.paths.databasePathHelper",return_value=p.name)
+            mocker.patch("ofscraper.utils.paths.databasePathHelper",return_value=p.name)
             create_profile_table("11111","test")
             write_profile_table("11112","test")
--- test/db/stories_test.py
+++ test/db/stories_test.py
@@ -7,7 +7,7 @@ from ofscraper.api.posts import Post,Media
 def test_stories_create(mocker):
     with tempfile.NamedTemporaryFile() as p:
         try:
-            mocker.patch("src.utils.paths.databasePathHelper",return_value=p.name)
+            mocker.patch("ofscraper.utils.paths.databasePathHelper",return_value=p.name)
             create_stories_table("11111","test")
         except:
             raise Exception
@@ -17,14 +17,14 @@ def test_stories_create(mocker):
 def test_stories_failure(mocker):
     with tempfile.NamedTemporaryFile() as p:   
         with pytest.raises(Exception):
-            mocker.patch("src.utils.paths.databasePathHelper",return_value=p.name)
+            mocker.patch("ofscraper.utils.paths.databasePathHelper",return_value=p.name)
             create_stories_table("11111")
 
 
 def test_stories_insert(mocker):
     with tempfile.NamedTemporaryFile() as p:
         try:
-            mocker.patch("src.utils.paths.databasePathHelper",return_value=p.name)
+            mocker.patch("ofscraper.utils.paths.databasePathHelper",return_value=p.name)
             create_stories_table("11111","test")
             write_stories_table(Post(STORIES_EXAMPLE,"11111","test"),"11111","test")
         except Exception as E:
@@ -33,7 +33,7 @@ def test_stories_insert(mocker):
 def test_stories_insert_failure(mocker):
     with tempfile.NamedTemporaryFile() as p:   
         with pytest.raises(Exception):
-            mocker.patch("src.utils.paths.databasePathHelper",return_value=p.name)
+            mocker.patch("ofscraper.utils.paths.databasePathHelper",return_value=p.name)
             create_stories_table("11111","test")
             write_stories_table(Post(STORIES_EXAMPLE,"111","test2"))
 
