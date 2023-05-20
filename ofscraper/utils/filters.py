dbfdbd03436c1d8dc3aa4df3752e3742428b969e
datawhores <67020411+datawhores@users.noreply.github.com>
2023-05-20 14:05:40 -0500

    add support for dates

--- ofscraper/utils/filters.py
+++ ofscraper/utils/filters.py
@@ -1,13 +1,16 @@
 
 import logging
 import arrow
+import ofscraper.utils.config as config
+import ofscraper.utils.args as args_
 
-
+args=args_.getargs()
 log=logging.getLogger(__package__)
 def filterMedia(media):
     media=dupefilter(media)
     media=datesorter(media)
     media=posts_type_filter(media)
+    media=posts_date_filter(media)
     return media
 
 def date_filter(media):
@@ -30,6 +33,7 @@ def datesorter(output):
     return list(sorted(output,key=lambda x:x.date,reverse=True))
 
 def posts_type_filter(media): 
+    filtersettings=config.get_filter(config.read_config())
     if isinstance(filtersettings,str):
         filtersettings=filtersettings.split(",")
     if isinstance(filtersettings,list):
@@ -38,8 +42,15 @@ def posts_type_filter(media):
         if len(filtersettings)==0:
             return media
         log.info(f"filtering Media to {','.join(filtersettings)}")
-        output= list(filter(lambda x:x.mediatype.lower() in filtersettings,output))
+        media =list(filter(lambda x:x.mediatype.lower() in filtersettings,media))
     else:
         log.info("The settings you picked for the filter are not valid\nNot Filtering")
-        log.debug(f"[bold]Combined Media Count Filtered:[/bold] {len(output)}")
-    return output
+        log.debug(f"[bold]Combined Media Count Filtered:[/bold] {len(media)}")
+    return media
+
+def posts_date_filter(media):
+    if args.before:
+        media=filter(lambda x:x.postdate==None or arrow.get(x.postdate)<=args.before,media)
+    if args.after:
+        media=filter(lambda x:x.postdate==None or arrow.get(x.postdate)>=args.after,media)
+    return list(media)
