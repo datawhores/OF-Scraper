e8afb8bf5ebd7b09a9a7a4381906f2db6c171517
datawhores <67020411+datawhores@users.noreply.github.com>
2023-05-24 17:21:46 -0500

    add rich to messages
    Former-commit-id: fa08f9840db99bfa3eb5df8f1106860b73452985

--- ofscraper/api/messages.py
+++ ofscraper/api/messages.py
@@ -12,12 +12,24 @@ import time
 import httpx
 import logging
 from tenacity import retry,stop_after_attempt,wait_random
-from tqdm.asyncio import tqdm
+from rich.progress import Progress
+from rich.progress import (
+    Progress,
+    TextColumn,
+    SpinnerColumn
+)
+from rich.panel import Panel
+from rich.console import Group
+from rich.live import Live
+from rich.style import Style
+import arrow
 import arrow
 import ofscraper.constants as constants
 import ofscraper.utils.auth as auth
 import ofscraper.utils.paths as paths
 from ..utils import auth
+import ofscraper.utils.console as console
+
 from diskcache import Cache
 cache = Cache(paths.getcachepath())
 log=logging.getLogger(__package__)
@@ -28,38 +40,45 @@ log=logging.getLogger(__package__)
 async def get_messages(headers, model_id):
     global sem
     sem = asyncio.Semaphore(8)
-    oldmessages=cache.get(f"messages_{model_id}",default=[]) 
-    log.debug(f"[bold]Messages Cache[/bold] {len(oldmessages)} found")
-    
-    postedAtArray=list(map(lambda x:x["id"],sorted(oldmessages,key=lambda x:arrow.get(x["createdAt"]).float_timestamp,reverse=True)))
-    global tasks
-    tasks=[]
-    
-    #split and interval can't match because of breakpoints
-    split=40
-    interval=30
-    if len(postedAtArray)>split:
-        splitArrays=[postedAtArray[i:i+split] for i in range(0, len(postedAtArray), interval)]
-        tasks.append(asyncio.create_task(scrape_messages(headers,model_id)))
-        tasks.extend(list(map(lambda x:asyncio.create_task(scrape_messages(headers,model_id,message_id=x[0])),splitArrays[1:-1])))
-        tasks.append(asyncio.create_task(scrape_messages(headers,model_id,message_id=splitArrays[-1][0],recursive=True)))
-    else:
-        tasks.append(asyncio.create_task(scrape_messages(headers,model_id,recursive=True)))
-   
-  
-    
-    
-    responseArray=[]
-    page_count=0 
-    desc = 'Pages Progress: {page_count}'   
+    overall_progress=Progress(SpinnerColumn(style=Style(color="blue"),),TextColumn("Getting Messages...\n{task.description}"))
+    job_progress=Progress("{task.description}")
+    progress_group = Group(
+    overall_progress,
+    Panel(Group(job_progress)))
+    with Live(progress_group, refresh_per_second=10,console=console.shared_console): 
 
-    with tqdm(desc=desc.format(page_count=page_count), colour='cyan',position=2,disable=True if logging.getLogger("ofscraper").handlers[1].level>=constants.SUPPRESS_LOG_LEVEL else False) as main_bar:
+        oldmessages=cache.get(f"messages_{model_id}",default=[]) 
+        oldmsgset=set(map(lambda x:x.get("id"),oldmessages))
+        log.debug(f"[bold]Messages Cache[/bold] {len(oldmessages)} found")
+        
+        postedAtArray=list(map(lambda x:x["id"],sorted(oldmessages,key=lambda x:arrow.get(x["createdAt"]).float_timestamp,reverse=True)))
+        global tasks
+        tasks=[]
+        
+        #split and interval can't match because of breakpoints
+        split=40
+        interval=30
+        if len(postedAtArray)>split:
+            splitArrays=[postedAtArray[i:i+split] for i in range(0, len(postedAtArray), interval)]
+            tasks.append(asyncio.create_task(scrape_messages(headers,model_id,job_progress)))
+            tasks.extend(list(map(lambda x:asyncio.create_task(scrape_messages(headers,model_id,job_progress,message_id=x[0])),splitArrays[1:-1])))
+            tasks.append(asyncio.create_task(scrape_messages(headers,model_id,job_progress,message_id=splitArrays[-1][0],recursive=True)))
+        else:
+            tasks.append(asyncio.create_task(scrape_messages(headers,model_id,job_progress,recursive=True)))
+    
+    
+        
+        
+        responseArray=[]
+        page_count=0 
+        page_task = overall_progress.add_task(f' Pages Progress: {page_count}',visible=False if logging.getLogger("ofscraper").handlers[1].level>=constants.SUPPRESS_LOG_LEVEL else True)
+
+
         while len(tasks)!=0:
                     for coro in asyncio.as_completed(tasks):
                         result=await coro or []
                         page_count=page_count+1
-                        main_bar.set_description(desc.format(page_count=page_count), refresh=False)
-                        main_bar.update()
+                        overall_progress.update(page_task,description=f'Pages Progress: {page_count}')
                         responseArray.extend(result)
                     time.sleep(2)
                     tasks=list(filter(lambda x:x.done()==False,tasks))
@@ -70,19 +89,25 @@ async def get_messages(headers, model_id):
         if message["id"] in dupeSet:
             continue
         dupeSet.add(message["id"])
-        unduped.append(message)       
-    log.debug(f"[bold]Messages Count without Dupes[/bold] {len(unduped)} found")
-    cache.set(f"messages_{model_id}",unduped,expire=constants.RESPONSE_EXPIRY) 
-    cache.close()
-    return unduped
+        oldmsgset.discard(message["id"])       
+        unduped.append(message)
+    if len(oldmsgset)==0:
+        cache.set(f"timeline_{model_id}",unduped,expire=constants.RESPONSE_EXPIRY)
+        cache.close()
+    else:
+        log.debug("Some post where not retrived not setting cache")
+
+    return unduped    
 
 @retry(stop=stop_after_attempt(constants.NUM_TRIES),wait=wait_random(min=5, max=20),reraise=True)   
-async def scrape_messages(headers, user_id, message_id=None,recursive=False) -> list:
+async def scrape_messages(headers, user_id, progess,message_id=None,recursive=False) -> list:
+    global sem
     ep = constants.messagesNextEP if message_id else constants.messagesEP
     url = ep.format(user_id, message_id)
     async with sem:
         async with httpx.AsyncClient(http2=True, headers=headers) as c:
             auth.add_cookies(c)
+            log.debug(url)
             c.headers.update(auth.create_sign(url, headers))
             r = await c.get(url, timeout=None)
             if not r.is_error:
