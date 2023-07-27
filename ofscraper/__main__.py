eb43be7204a986aa72992efe5280eb8eeeea8c05
datawhores <datawhores@users.noreply.github.com>
2023-07-27 14:01:54 -0500

    modify entry delete init.py

--- ofscraper/__main__.py
+++ ofscraper/__main__.py
@@ -2,7 +2,11 @@
 import multiprocessing
 import sys
 import ofscraper.start as start
+def main():
+    sys.exit(start.main())
 
-multiprocessing.freeze_support()
-sys.exit(start.main())
-
+# protect the entry point
+if __name__ == '__main__':
+	# enable support for multiprocessing
+    multiprocessing.freeze_support()
+    main()
