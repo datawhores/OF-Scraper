af8e897cf2766fb36178b340e80ad0133091ae09
datawhores <datawhores@users.noreply.github.com>
2023-08-01 06:41:12 -0500

    remove headers from chain

--- ofscraper/api/init.py
+++ ofscraper/api/init.py
@@ -18,18 +18,18 @@ log=logging.getLogger("shared")
 
 
 console=Console()
-def print_sign_status(headers):
+def print_sign_status():
     with stdout.lowstdout():
-        status=getstatus(headers)
+        status=getstatus()
         if status=="UP":
             print('Status - \033[32mUP\033[0m')
         else:
             print('Status - \033[31mDOWN\033[0m')
 
 
-def getstatus(headers):
+def getstatus():
     try:
-        me.scrape_user(headers)
+        me.scrape_user()
         return "UP"
     except Exception as e:
         log.traceback(e)
