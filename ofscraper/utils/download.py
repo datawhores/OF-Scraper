469ef712907d291648203c55e593ce1db5e2bc08
datawhores <datawhores@users.noreply.github.com>
2023-08-06 20:22:50 -0500

    use skip and failed in desc as well

--- ofscraper/utils/download.py
+++ ofscraper/utils/download.py
@@ -96,7 +96,7 @@ def reset_globals():
     global total_data
     total_data=0
     global desc
-    desc = 'Progress: ({p_count} photos, {v_count} videos, {a_count} audios,  {skipped} failed, {forced_skipped} forced skipped || {sumcount}/{mediacount}||{data}/{total})'   
+    desc = 'Progress: ({p_count} photos, {v_count} videos, {a_count} audios, {forced_skipped} skipped, {skipped} failed || {sumcount}/{mediacount}||{data}/{total})'   
     global count_lock
     count_lock=aioprocessing.AioLock()
     global dir_lock
