21c3d08b42b6b758d634bb0384e8859824c7d0c1
datawhores <datawhores@users.noreply.github.com>
2023-11-27 14:02:31 -0600

    make ffmpeg for mac dynamic

--- ofscraper/constants.py
+++ ofscraper/constants.py
@@ -157,7 +157,7 @@ MP4DECRYPT_WINDOWS="https://www.bok.net/Bento4/binaries/Bento4-SDK-1-6-0-640.x86
 MP4DECRYPT_MAC="https://www.bok.net/Bento4/binaries/Bento4-SDK-1-6-0-640.universal-apple-macosx.zip"
 FFMPEG_LINUX="https://github.com/BtbN/FFmpeg-Builds/releases/download/latest/ffmpeg-master-latest-linux64-gpl.tar.xz"
 FFMPEG_WINDOWS="https://github.com/BtbN/FFmpeg-Builds/releases/download/latest/ffmpeg-master-latest-win64-gpl.zip"
-FFMPEG_MAC="https://evermeet.cx/ffmpeg/ffmpeg-111111-gc44fe10160.zip"
+FFMPEG_MAC="https://evermeet.cx/ffmpeg/#api-download"
 
 
 MAX_SEMAPHORE=14
