66c9a4ca466ed3070fe35d44c18729162a609493
datawhores <datawhores@users.noreply.github.com>
2023-12-17 07:19:52 -0600

    add price min max filters

--- ofscraper/filters/models/price.py
+++ ofscraper/filters/models/price.py
@@ -69,4 +69,86 @@ def pricePaidFreeFilterHelper(filterusername):
             )
         )
         log.debug(f"free promo filter username count: {len(filterusername)}")
+    filterusername = priceMinMaxFilters(filterusername)
+
+    return filterusername
+
+
+def priceMinMaxFilters(filterusername):
+    log = logging.getLogger("shared")
+    log.debug(f"Promo Price min Filter: {args_.getargs().promo_price_min}")
+    if args_.getargs().promo_price_min:
+        filterusername = list(
+            filter(
+                lambda x: x.final_promo_price >= args_.getargs().promo_price_min,
+                filterusername,
+            )
+        )
+        log.debug(f"currently promo min filter: {len(filterusername)}")
+    log.debug(f"Promo Price max Filter: {args_.getargs().promo_price_max}")
+    if args_.getargs().promo_price_max:
+        filterusername = list(
+            filter(
+                lambda x: x.final_promo_price <= args_.getargs().promo_price_max,
+                filterusername,
+            )
+        )
+        log.debug(f"currently promo max filter: {len(filterusername)}")
+
+    log.debug(f"Regular Price min Filter: {args_.getargs().regular_price_min}")
+    if args_.getargs().regular_price_min:
+        filterusername = list(
+            filter(
+                lambda x: x.final_regular_price >= args_.getargs().regular_price_min,
+                filterusername,
+            )
+        )
+        log.debug(f"currently regular min filter: {len(filterusername)}")
+    log.debug(f"Regular Price max Filter: {args_.getargs().regular_price_max}")
+    if args_.getargs().regular_price_max:
+        filterusername = list(
+            filter(
+                lambda x: x.final_regular_price <= args_.getargs().regular_price_max,
+                filterusername,
+            )
+        )
+        log.debug(f"currently regular max filter: {len(filterusername)}")
+
+    log.debug(f"Renewal Price min Filter: {args_.getargs().renewal_price_min}")
+    if args_.getargs().renewal_price_min:
+        filterusername = list(
+            filter(
+                lambda x: x.final_renewal_price >= args_.getargs().renewal_price_min,
+                filterusername,
+            )
+        )
+        log.debug(f"currently renewal min filter: {len(filterusername)}")
+    log.debug(f"Renewal Price max Filter: {args_.getargs().renewal_price_max}")
+    if args_.getargs().renewal_price_max:
+        filterusername = list(
+            filter(
+                lambda x: x.final_renewal_price <= args_.getargs().renewal_price_max,
+                filterusername,
+            )
+        )
+        log.debug(f"currently renewal max filter: {len(filterusername)}")
+
+    log.debug(f"Current Price min Filter: {args_.getargs().current_price_min}")
+    if args_.getargs().current_price_min:
+        filterusername = list(
+            filter(
+                lambda x: x.final_current_price >= args_.getargs().current_price_min,
+                filterusername,
+            )
+        )
+        log.debug(f"currently current min filter: {len(filterusername)}")
+    log.debug(f"Current Price max Filter: {args_.getargs().current_price_max}")
+    if args_.getargs().current_price_max:
+        filterusername = list(
+            filter(
+                lambda x: x.final_current_price <= args_.getargs().current_price_max,
+                filterusername,
+            )
+        )
+        log.debug(f"currently current max filter: {len(filterusername)}")
     return filterusername
