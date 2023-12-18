6abab8997350514e3b367bab32700947b3aef0ef
datawhores <datawhores@users.noreply.github.com>
2023-12-18 13:38:05 -0600

    use mutual execulsion groups with some filters reword args a bit reorg

--- ofscraper/filters/models/flags.py
+++ ofscraper/filters/models/flags.py
@@ -7,40 +7,40 @@ def promoFilterHelper(filterusername):
     log = logging.getLogger("shared")
 
     log.debug(f"Promo Flag: {args_.getargs().promo}")
-    if args_.getargs().promo == "yes":
+    if args_.getargs().promo:
         filterusername = list(
             filter(lambda x: x.lowest_promo_claim is not None, filterusername)
         )
         log.debug(f"Promo Flag Yes Count: {len(filterusername)}")
 
-    elif args_.getargs().promo == "no":
+    elif args_.getargs().promo is False:
         filterusername = list(
             filter(lambda x: x.lowest_promo_claim is None, filterusername)
         )
         log.debug(f"Promo Flag No Count: {len(filterusername)}")
 
     log.debug(f"All Promo Flag: {args_.getargs().all_promo}")
-    if args_.getargs().all_promo == "yes":
+    if args_.getargs().all_promo:
         filterusername = list(
             filter(lambda x: x.lowest_promo_all is not None, filterusername)
         )
         log.debug(f"All Promo Flag Yes Count: {len(filterusername)}")
 
-    elif args_.getargs().all_promo == "no":
+    elif args_.getargs().all_promo is False:
         filterusername = list(
             filter(lambda x: x.lowest_promo_all is None, filterusername)
         )
         log.debug(f"All Promo Flag No Count: {len(filterusername)}")
     log.debug(f"Last Seen Flag: {args_.getargs().last_seen}")
-    if args_.getargs().last_seen == "yes":
+    if args_.getargs().last_seen:
         filterusername = list(filter(lambda x: x.last_seen is not None, filterusername))
         log.debug(f"Last Seen Flag Yes Count: {len(filterusername)}")
 
-    elif args_.getargs().last_seen == "no":
+    elif args_.getargs().last_seen is False:
         filterusername = list(filter(lambda x: x.last_seen is None, filterusername))
         log.debug(f"Last Seen Flag No Count: {len(filterusername)}")
         log.debug(f"Last Seen Flag: {args_.getargs().last_seen}")
-    if args_.getargs().free_trail == "yes":
+    if args_.getargs().free_trail:
         filterusername = list(
             filter(
                 lambda x: (
@@ -52,7 +52,7 @@ def promoFilterHelper(filterusername):
         )
         log.debug(f"Free Trail Flag Yes Count: {len(filterusername)}")
 
-    elif args_.getargs().free_trail == "no":
+    elif args_.getargs().free_trail is False:
         filterusername = list(
             filter(
                 lambda x: (x.final_current_price > 0 or x.final_regular_price == 0),
