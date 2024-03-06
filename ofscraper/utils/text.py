7c7ef68c2433a7037f3304a7809f61a81eba3ea4
datawhores <datawhores@users.noreply.github.com>
2024-03-06 15:30:51 -0600

    overwrite if dupe

--- ofscraper/utils/text.py
+++ ofscraper/utils/text.py
@@ -9,6 +9,7 @@ from concurrent.futures import ThreadPoolExecutor
 import aiofiles
 
 import ofscraper.classes.placeholder as placeholder
+import ofscraper.utils.args.read as read_args
 import ofscraper.utils.constants as constants
 import ofscraper.utils.settings as settings
 from ofscraper.utils.context.run_async import run
@@ -20,11 +21,12 @@ async def get_text(values):
         max_workers=constants.getattr("MAX_TEXT_WORKER")
     ) as executor:
         asyncio.get_event_loop().set_default_executor(executor)
+        dupe = read_args.retriveArgs().dupe
         async with asyncio.TaskGroup() as tg:
-            [tg.create_task(get_text_process(value)) for value in values]
+            [tg.create_task(get_text_process(value, dupe=dupe)) for value in values]
 
 
-async def get_text_process(ele):
+async def get_text_process(ele, dupe=None):
     log = logging.getLogger("shared")
     try:
         if "Text" not in settings.get_mediatypes():
@@ -37,7 +39,7 @@ async def get_text_process(ele):
 
         placeholderObj = placeholder.Placeholders(new_ele, "txt")
         await placeholderObj.init()
-        if pathlib.Path(placeholderObj.filepath).exists():
+        if pathlib.Path(placeholderObj.filepath).exists() and not dupe:
             return
         wrapped_text = textwrap.wrap(
             new_ele.text, width=constants.getattr("MAX_TEXT_LENGTH")
