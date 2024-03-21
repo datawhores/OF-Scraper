43cebde48115ba261156aeeed4f841dd056451a9
datawhores <datawhores@users.noreply.github.com>
2024-03-20 19:00:32 -0500

    indenting

--- ofscraper/api/subscriptions/lists.py
+++ ofscraper/api/subscriptions/lists.py
@@ -108,30 +108,30 @@ async def get_lists():
                 page_task = overall_progress.add_task(
                     f"UserList Pages Progress: {page_count}", visible=True
                 )
-            while bool(tasks):
-                new_tasks = []
-                try:
-                    async with asyncio.timeout(
-                        constants.getattr("API_TIMEOUT_PER_TASKS") * len(tasks)
-                    ):
-                        for task in asyncio.as_completed(tasks):
-                            try:
-                                result, new_tasks_batch = await task
-                                new_tasks.extend(new_tasks_batch)
-                                page_count = page_count + 1
-                                overall_progress.update(
-                                    page_task,
-                                    description=f"UserList Pages Progress: {page_count}",
-                                )
-                                output.extend(result)
-                            except Exception as E:
-                                log.traceback_(E)
-                                log.traceback_(traceback.format_exc())
-                                continue
-                except TimeoutError as E:
-                    log.traceback_(E)
-                    log.traceback_(traceback.format_exc())
-                tasks = new_tasks
+                while bool(tasks):
+                    new_tasks = []
+                    try:
+                        async with asyncio.timeout(
+                            constants.getattr("API_TIMEOUT_PER_TASKS") * len(tasks)
+                        ):
+                            for task in asyncio.as_completed(tasks):
+                                try:
+                                    result, new_tasks_batch = await task
+                                    new_tasks.extend(new_tasks_batch)
+                                    page_count = page_count + 1
+                                    overall_progress.update(
+                                        page_task,
+                                        description=f"UserList Pages Progress: {page_count}",
+                                    )
+                                    output.extend(result)
+                                except Exception as E:
+                                    log.traceback_(E)
+                                    log.traceback_(traceback.format_exc())
+                                    continue
+                    except TimeoutError as E:
+                        log.traceback_(E)
+                        log.traceback_(traceback.format_exc())
+                    tasks = new_tasks
 
         overall_progress.remove_task(page_task)
         log.trace(
