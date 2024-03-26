8c0401f5cebef003eaac43374d38d481bcfb7761
datawhores <datawhores@users.noreply.github.com>
2024-03-26 08:28:34 -0500

    fixes from newer verisons

--- ofscraper/classes/table.py
+++ ofscraper/classes/table.py
@@ -7,15 +7,12 @@ from rich.text import Text
 from textual import events
 from textual.app import App, ComposeResult
 from textual.containers import Horizontal, Vertical, VerticalScroll
-from textual.widgets import (
-    Button,
-    Checkbox,
-    ContentSwitcher,
-    DataTable,
-    Input,
-    Label,
-    TextLog,
-)
+from textual.widgets import Button, Checkbox, ContentSwitcher, DataTable, Input, Label
+
+try:
+    from textual.widgets import TextLog
+except ImportError:
+    from textual.widgets import RichLog as TextLog
 
 import ofscraper.utils.console as console_
 import ofscraper.utils.logger as logger
