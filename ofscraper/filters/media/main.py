6729953d89be9b7e29df8d4b2c6c7cb90aedb1a9
datawhores <datawhores@users.noreply.github.com>
2024-04-18 17:10:30 -0500

    split more logs

--- ofscraper/filters/media/main.py
+++ ofscraper/filters/media/main.py
@@ -81,162 +81,60 @@ def filterMedia(media):
 
 def filterPost(post):
     count = 1
-    logformater = "{} data: {} postid: {}"
-    log.trace(
-        "\n\n\n".join(
-            map(
-                lambda x: logformater.format(
-                    f"filter {count}-> all post no filter:",
-                    x.post,
-                    x.id,
-                ),
-                post,
-            )
-        )
-    )
+    helpers.trace_log(count,post,"all post no filter:")
+
     log.debug(f"filter {count}-> all post no filter count: {len(post)}")
     post = helpers.sort_media(post)
     count += 1
-    log.trace(
-        "\n\n\n".join(
-            map(
-                lambda x: logformater.format(
-                    f"filter {count}-> final post  from retrived post: ",
-                    x.post,
-                    x.id,
-                ),
-                post,
-            )
-        )
-    )
+    helpers.trace_log(count,post,"final post  from retrived post:")
+
     log.debug(f"filter {count}->  final post count from retrived post: {len(post)}")
     post = helpers.dupefilter(post)
     count += 1
-    log.trace(
-        "\n\n\n".join(
-            map(
-                lambda x: logformater.format(
-                    f"filter {count}-> all post dupe filter: ",
-                    x.post,
-                    x.id,
-                ),
-                post,
-            )
-        )
-    )
+    helpers.trace_log(count,post,"all post dupe filter:")
+    
 
     log.debug(f"filter {count}-> all post dupe filter count: {len(post)}")
     post = helpers.post_datesorter(post)
     count += 1
+    helpers.trace_log(count,post,"all post datesort:")
 
-    log.trace(
-        "\n\n\n".join(
-            map(
-                lambda x: logformater.format(
-                    "filter 3-> all post datesort: ", x.post, x.id
-                ),
-                post,
-            )
-        )
-    )
     log.debug(f"filter {count}-> all post datesort count: {len(post)}")
     post = helpers.posts_date_filter(post)
     count += 1
-    log.trace(
-        "\n\n\n".join(
-            map(
-                lambda x: logformater.format(
-                    f"filter {count}-> all post post date filter: ",
-                    x.post,
-                    x.id,
-                ),
-                post,
-            )
-        )
-    )
+    helpers.trace_log(count,post,"all post post date filter:")
+
     log.debug(f"filter {count}-> all post post date filter: {len(post)}")
     post = helpers.post_timed_filter(post)
     count += 1
+    helpers.trace_log(count,post,"all post post timed post filter:")
 
-    log.trace(
-        "\n\n\n".join(
-            map(
-                lambda x: logformater.format(
-                    f"filter {count}->  all post post timed post filter: ",
-                    x.post,
-                    x.id,
-                ),
-                post,
-            )
-        )
-    )
+
     log.debug(f"filter {count}->  all post post timed post filter count: {len(post)}")
     post = helpers.post_user_filter(post)
     count += 1
+    helpers.trace_log(count,post,"all post post included text filter:")
 
-    log.trace(
-        "\n\n\n".join(
-            map(
-                lambda x: logformater.format(
-                    f"filter {count}-> all post post included text filter: ",
-                    x.post,
-                    x.id,
-                ),
-                post,
-            )
-        )
-    )
+
     log.debug(
         f"filter {count}->  all post post included text filter count: {len(post)}"
     )
     post = helpers.anti_post_user_filter(post)
     count += 1
+    helpers.trace_log(count,post,"all post post excluded text filter:")
 
-    log.trace(
-        "\n\n\n".join(
-            map(
-                lambda x: logformater.format(
-                    f"filter {count}-> all post post excluded text filter: ",
-                    x.post,
-                    x.id,
-                ),
-                post,
-            )
-        )
-    )
+
     log.debug(
         f"filter {count}->  all post post excluded text filter count: {len(post)}"
     )
 
     post = helpers.mass_msg_filter(post)
     count += 1
-    log.trace(
-        "\n\n\n".join(
-            map(
-                lambda x: logformater.format(
-                    f"filter {count}->  mass message filter: ",
-                    x.post,
-                    x.id,
-                ),
-                post,
-            )
-        )
-    )
+    helpers.trace_log(count,post,"mass message filter:")
+
     post = helpers.final_post_sort(post)
     count += 1
-    log.trace(
-        "\n\n\n".join(
-            map(
-                lambda x: logformater.format(
-                    f"filter {count}->  all post final sort: ",
-                    x.post,
-                    x.id,
-                ),
-                post,
-            )
-        )
-    )
-    log.debug(f"filter{count}-> all post final sort count {len(post)}")
+    helpers.trace_log(count,post,"all post final sort:")
 
     return post
 
