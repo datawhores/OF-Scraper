fd3228ef7e34d6cf893f701dda24f6520a441e0b
datawhores <datawhores@users.noreply.github.com>
2024-04-23 03:15:38 -0500

    move line

--- ofscraper/db/operations_/merge.py
+++ ofscraper/db/operations_/merge.py
@@ -59,9 +59,9 @@ async def batch_database_changes(new_root, old_root):
     
     await create_tables(db_path=new_db_path)
     for ele in paths.get_all_db(old_root):
-        log.info(f"Merging {ele} with {new_db_path}")
         if ele == new_db_path:
             continue
+        log.info(f"Merging {ele} with {new_db_path}")
         try:
             model_id = get_single_model_via_profile(db_path=ele)
             if not model_id:
