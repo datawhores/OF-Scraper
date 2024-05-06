ab9fe810bd5add2bcdb052b1531977445a6f5fe8
datawhores <datawhores@users.noreply.github.com>
2024-05-06 11:18:00 -0500

    fix getting my profile in placeholder

--- ofscraper/utils/me.py
+++ ofscraper/utils/me.py
@@ -3,11 +3,11 @@ from rich.console import Console
 import ofscraper.utils.context.stdout as stdout
 import ofscraper.utils.encoding as encoding
 import ofscraper.api.me as me
+import ofscraper.utils.profiles.data as profile_data
 
 
 def parse_user():
-    profile = me.scrape_user()
-
+    profile = profile_data.get_my_info()
     name = encoding.encode_utf_16(profile["name"])
     username = profile["username"]
     return (name, username)
