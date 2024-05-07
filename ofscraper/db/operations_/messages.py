e65501e0b4aff758340b753898a0f944ef8c8825
datawhores <datawhores@users.noreply.github.com>
2024-05-07 06:17:11 -0500

    test

--- ofscraper/db/operations_/messages.py
+++ ofscraper/db/operations_/messages.py
@@ -243,5 +243,4 @@ async def get_youngest_message_date(model_id=None, username=None, **kwargs):
     data = await media.get_messages_media(model_id=model_id, username=username)
     last_item = sorted(data, key=lambda x: arrow.get(x["posted_at"] or 0))[-1]
     print(last_item)
-    quit()
     return last_item["posted_at"]
