3bf54bc3915cf209e82ae0d9be25b28976d96f30
datawhores <datawhores@users.noreply.github.com>
2024-05-07 05:41:22 -0500

    change log

--- ofscraper/api/messages.py
+++ ofscraper/api/messages.py
@@ -514,11 +514,11 @@ def trace_log_old(responseArray):
 
 
 def log_after_before(after, before, username):
-    log.info(f"Messages before = {arrow.get(before).format(constants.getattr('API_DATE_FORMAT'))}")
 
     log.info(
         f"""
-Setting initial message scan date for {username} to {arrow.get(after).format(constants.getattr('API_DATE_FORMAT'))}
+Setting Message range for {username} from {arrow.get(after).format(constants.getattr('API_DATE_FORMAT'))} to {arrow.get(before).format(constants.getattr('API_DATE_FORMAT'))}
+
 [yellow]Hint: append ' --after 2000' to command to force scan of all messages + download of new files only[/yellow]
 [yellow]Hint: append ' --after 2000 --force-all' to command to force scan of all messages + download/re-download of all files[/yellow]
 
