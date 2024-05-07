7dcdcc9ea0a2fc6ae857bd7a4627af825b2df387
datawhores <datawhores@users.noreply.github.com>
2024-05-07 06:09:46 -0500

    get now

--- ofscraper/db/operations_/messages.py
+++ ofscraper/db/operations_/messages.py
@@ -236,10 +236,10 @@ async def make_messages_table_changes(
 async def get_oldest_message_date(model_id=None, username=None, **kwargs):
     data = await media.get_messages_media(model_id=model_id, username=username)
     last_item = sorted(data, key=lambda x: arrow.get(x["posted_at"] or 0))[0]
-    return last_item["posted_at"] or 0
+    return last_item["posted_at"] or arrow.now().float_timestamp-1000
 
 
 async def get_youngest_message_date(model_id=None, username=None, **kwargs):
     data = await media.get_messages_media(model_id=model_id, username=username)
     last_item = sorted(data, key=lambda x: arrow.get(x["posted_at"] or 0))[-1]
-    return last_item["posted_at"] or 0
+    return last_item["posted_at"] or arrow.now().float_timestamp-1000
