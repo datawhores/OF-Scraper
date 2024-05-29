6cf8b78bcd8f7a211e443ae1b3103ba8e20bf4f2
datawhores <datawhores@users.noreply.github.com>
2024-05-29 16:40:31 -0500

    common log for sessionbuilder

--- ofscraper/download/shared/classes/session.py
+++ ofscraper/download/shared/classes/session.py
@@ -11,16 +11,19 @@ class download_session(sessionManager.sessionManager):
         retries=None,
         wait_min=None,
         wait_max=None,
+        log=None
     ) -> None:
         sem = sem or common_globals.sem
         retries = retries or get_download_req_retries()
         wait_min = wait_min or constants.getattr("OF_MIN_WAIT_API")
         wait_max = wait_max or constants.getattr("OF_MAX_WAIT_API")
+        log=log or common_globals.log
         super().__init__(
             sem=sem,
             retries=retries,
             wait_min=wait_min,
             wait_max=wait_max,
+            log=log
         )
 
 
