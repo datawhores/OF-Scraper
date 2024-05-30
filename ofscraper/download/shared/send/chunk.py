a4c0ba8772720c7af39cf38c8f95bfa902a04f4b
datawhores <datawhores@users.noreply.github.com>
2024-05-30 07:59:03 -0500

    move para

--- ofscraper/download/shared/send/chunk.py
+++ ofscraper/download/shared/send/chunk.py
@@ -12,7 +12,7 @@ from ofscraper.utils.logs.helpers import getNumber
 def send_chunk_msg(ele,total,placeholderObj):
     msg= f"{get_medialog(ele)} Download Progress:{(pathlib.Path(placeholderObj.tempfilepath).absolute().stat().st_size)}/{total}"
     if constants.getattr("SHOW_DL_CHUNKS"):
-        common_globals.log.log(getNumber(constants.getattr("SHOW_DL_CHUNKS_LEVEL"),msg))
+        common_globals.log.log(getNumber(constants.getattr("SHOW_DL_CHUNKS_LEVEL")),msg)
     elif constants.getattr("SHOW_DL_CHUNKS"):
         common_globals.log.trace(msg)  
         
