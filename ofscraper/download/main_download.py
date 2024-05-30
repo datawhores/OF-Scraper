30fc068c5694381ee4a89a9f1daf47c22ad97dae
datawhores <datawhores@users.noreply.github.com>
2024-05-29 23:26:19 -0500

    f-string fix

--- ofscraper/download/main_download.py
+++ ofscraper/download/main_download.py
@@ -128,7 +128,7 @@ async def fresh_data_handler(c, tempholderObj, ele):
 
 async def resume_data_handler(data, c, tempholderObj, ele):
     common_globals.log.debug(f"{get_medialog(ele)} Data from cache{data}")
-    common_globals.log.debug(f"{get_medialog(ele)} Total size from cache {format_size(data.get("content-total")) if data.get("content-total") else "unknown"}")
+    common_globals.log.debug(f"{get_medialog(ele)} Total size from cache {format_size(data.get('content-total')) if data.get('content-total') else 'unknown'}")
     content_type = data.get("content-type").split("/")[-1]
     total = int(data.get("content-total")) if data.get("content-total") else None
     placeholderObj = await placeholder.Placeholders(ele, content_type).init()
