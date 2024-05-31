04d4958b5a0636e03254b86850ee575feedde51a
datawhores <datawhores@users.noreply.github.com>
2024-05-31 03:19:02 -0500

    update logs

--- ofscraper/classes/sessionmanager.py
+++ ofscraper/classes/sessionmanager.py
@@ -499,7 +499,7 @@ class sessionManager:
 
                     raise E
         yield r
-        log.debug(f"releasing async sem waiters:{len(sem._waiters) if sem._waiters else 0} | value: {sem._vasemlue}")
+        log.debug(f"releasing async sem waiters:{len(sem._waiters) if sem._waiters else 0} | value: {sem._value}")
         sem.release()
         log.debug(f"released async sem waiters:{len(sem._waiters) if sem._waiters else 0} | value: {sem._value}")
 
