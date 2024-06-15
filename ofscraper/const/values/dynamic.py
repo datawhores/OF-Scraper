8797e9f6af287af11e2d32efb2017bd67749ff34
datawhores <datawhores@users.noreply.github.com>
2024-06-14 22:11:41 -0500

    fix missing const

--- ofscraper/const/values/dynamic.py
+++ ofscraper/const/values/dynamic.py
@@ -10,7 +10,7 @@ DYNAMIC_OPTIONS_ALL=  DYNAMIC_OPTIONS + \
                 "digitals",
                 "manual",
                 "generic"]
-DYNAMIC_OPTIONS_DEFAULT="riley"
+DYNAMIC_RULE_DEFAULT="riley"
 
 DIGITALCRIMINALS = (
     "https://raw.githubusercontent.com/DATAHOARDERS/dynamic-rules/main/onlyfans.json"
