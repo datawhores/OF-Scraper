a59a6519c6fb11bbb6f8546e99b47da968687a59
datawhores <datawhores@users.noreply.github.com>
2024-06-23 12:13:18 -0500

    completey remove clear line

--- ofscraper/utils/live/empty.py
+++ ofscraper/utils/live/empty.py
@@ -10,7 +10,6 @@ import time
 def prompt_live():
     old_render = get_live().renderable
     stop_live()
-    # console.get_shared_console().clear()
     console.get_shared_console().clear_live()
     console.get_shared_console().line(2)
     #give time for screen to clear
