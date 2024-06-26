6cc7d73c583b1ea1c062a167e0c4e9788ebeb263
datawhores <datawhores@users.noreply.github.com>
2024-06-25 19:58:21 -0500

    print last log even if redudent

--- ofscraper/commands/helpers/final_log.py
+++ ofscraper/commands/helpers/final_log.py
@@ -4,16 +4,13 @@ import ofscraper.utils.constants as constants
 
 def final_log(data):
     log = logging.getLogger("shared") 
-    #give space to last long
-    if len(data)<3:
-        return
     elif constants.getattr("SHOW_RESULTS_LOG"):
         #give time for last long to show
         time.sleep(2)
         log.warning("\n\n\n")
         log.warning("[bold yellow]Final Results Logs[/bold yellow]")
         for record in data:
-                log.warning(record)
+            log.warning(record)
         log.warning("\n\n\n")
         #give time for last long to process
         time.sleep(3)
