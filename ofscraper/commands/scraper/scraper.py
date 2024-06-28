5f4560628b88141115c37c0c27bab9d5a638afb7
datawhores <datawhores@users.noreply.github.com>
2024-06-28 17:23:28 -0500

    move files again

--- ofscraper/commands/scraper/scraper.py
+++ ofscraper/commands/scraper/scraper.py
@@ -20,10 +20,37 @@ import ofscraper.utils.context.exit as exit
 import ofscraper.utils.paths.paths as paths
 import ofscraper.utils.run as run
 import ofscraper.utils.system.network as network
-from ofscraper.__version__ import __version__
 from ofscraper.commands.scraper.helpers.print import print_start
 from ofscraper.commands.scraper.helpers.prompt import process_prompts
-from ofscraper.commands.scraper.helpers.select import process_selected_areas
+import logging
+import ofscraper.commands.scraper.actions.download as download_action
+import ofscraper.utils.args.accessors.read as read_args
+import ofscraper.utils.context.exit as exit
+import ofscraper.utils.live.screens as progress_utils
+from ofscraper.commands.helpers.shared import run_action_bool
+
+from ofscraper.commands.helpers.final_log import final_log
+from ofscraper.commands.scraper.helpers.scrape_context import scrape_context_manager
+from ofscraper.utils.checkers import check_auth
+from ofscraper.commands.scraper.userfirst import process_users_actions_user_first
+from ofscraper.commands.scraper.normal import process_users_actions_normal
+from ofscraper.commands.scraper.helpers.prepare import prepare
+
+import logging
+
+import ofscraper.prompts.prompts as prompts
+import ofscraper.utils.config.data as data
+from ofscraper.__version__ import __version__
+import ofscraper.utils.menu as menu
+from ofscraper.commands.scraper.helpers.prompt import process_prompts
+import ofscraper.api.init as init
+import ofscraper.classes.sessionmanager.ofsession as sessionManager
+import ofscraper.commands.scraper.actions.download as download_action
+import ofscraper.models.selector as userselector
+import ofscraper.utils.actions as actions
+import ofscraper.utils.constants as constants
+import ofscraper.utils.profiles.tools as profile_tools
+log = logging.getLogger("shared")
 
 
 log = logging.getLogger("shared")
@@ -79,5 +106,60 @@ def scrapper():
     elif len(args.action) == 0:
         process_prompts()
 
+def process_selected_areas():
+    log.debug("[bold deep_sky_blue2] Running Action Mode [/bold deep_sky_blue2]")
+    runner()
+    while True:
+        if not data.get_InfiniteLoop() or prompts.continue_prompt() == "No":
+            break
+        action = prompts.action_prompt()
+        if action == "main":
+            process_prompts()
+            break
+        elif action == "quit":
+            break
+        else:
+            menu.get_count() > 0 and menu.reset_menu_helper()
+            runner()
+            menu.update_count()       
+
+@exit.exit_wrapper
+def runner(menu=False):
+    check_auth()
+    with scrape_context_manager(
+    ):
+        normal_data=[]
+        user_first_data=[
+        ]
+        scrape_paid_data=[]
+        with progress_utils.setup_activity_group_live(setup=True,revert=False,stop=True):
+            if read_args.retriveArgs().scrape_paid:
+                scrape_paid_data=download_action.scrape_paid_all()
+
+            if not run_action_bool():
+                pass
+
+            elif read_args.retriveArgs().users_first:
+                userdata, session = prepare(menu=menu)
+                user_first_data=process_users_actions_user_first(userdata, session)
+            else:
+                userdata, session = prepare()
+                normal_data=process_users_actions_normal(userdata, session)
+        final_log(normal_data+scrape_paid_data+user_first_data)
+
+def prepare(menu=False):
+    session = sessionManager.OFSessionManager(
+        sem=constants.getattr("API_REQ_SEM_MAX"),
+        total_timeout=constants.getattr("API_TIMEOUT_PER_TASK"),
+    )
+
+    download_action.unique_name_warning()
+    profile_tools.print_current_profile()
+    init.print_sign_status()
+    actions.select_areas()
+    if menu==True:
+        actions.set_scrape_paid()
 
 
+    userdata = userselector.getselected_usernames(rescan=False)
+    return userdata, session
