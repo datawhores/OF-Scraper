a5b74d6c4cae03a3c9d65965b79498e95eacb6ef
datawhores <datawhores@users.noreply.github.com>
2024-07-04 17:37:21 -0500

    fix ffmpeg file already exists error

--- ofscraper/download/utils/keyhelpers.py
+++ ofscraper/download/utils/keyhelpers.py
@@ -30,64 +30,68 @@ def setLog(input_):
 
 
 async def un_encrypt(item, c, ele, input_=None):
-    setLog(input_ or common_globals.log)
-    key = None
-    keymode = settings.get_key_mode()
-    past_key = (
-        await asyncio.get_event_loop().run_in_executor(
-            common_globals.thread, partial(cache.get, ele.license)
+    try:
+        setLog(input_ or common_globals.log)
+        key = None
+        keymode = settings.get_key_mode()
+        past_key = (
+            await asyncio.get_event_loop().run_in_executor(
+                common_globals.thread, partial(cache.get, ele.license)
+            )
+            if constants.getattr("USE_WIV_CACHE_KEY")
+            else None
         )
-        if constants.getattr("USE_WIV_CACHE_KEY")
-        else None
-    )
-    if past_key:
-        key = past_key
-        log.debug(f"{get_medialog(ele)} got key from cache")
-    elif keymode == "manual":
-        key = await key_helper_manual(c, item["pssh"], ele.license, ele.id)
-    elif keymode == "keydb":
-        key = await key_helper_keydb(c, item["pssh"], ele.license, ele.id)
-    elif keymode == "cdrm":
-        key = await key_helper_cdrm(c, item["pssh"], ele.license, ele.id)
-    elif keymode == "cdrm2":
-        key = await key_helper_cdrm2(c, item["pssh"], ele.license, ele.id)
-    if not key:
-        raise Exception(f"{get_medialog(ele)} Could not get key")
-    await asyncio.get_event_loop().run_in_executor(
-        common_globals.thread,
-        partial(cache.set, ele.license, key, expire=constants.getattr("KEY_EXPIRY")),
-    )
-    log.debug(f"{get_medialog(ele)} got key")
-    newpath = pathlib.Path(
-        re.sub("\.part$", "", str(item["path"]), flags=re.IGNORECASE)
-    )
-    log.debug(
-        f"{get_medialog(ele)}  renaming {pathlib.Path(item['path']).absolute()} -> {newpath}"
-    )
-    ffmpeg_key = get_ffmpeg_key(key)
-    r = subprocess.run(
-        [
-            settings.get_ffmpeg(),
-            "-decryption_key",
-            ffmpeg_key,
-            "-i",
-            str(item["path"]),
-            "-codec",
-            "copy",
-            str(newpath),
-        ],
-        stdout=subprocess.PIPE,
-        stderr=subprocess.PIPE,
-    )
-    if not pathlib.Path(newpath).exists():
-        log.debug(f"{get_medialog(ele)} ffmpeg decryption failed")
-        log.debug(f"{get_medialog(ele)} ffmpeg {r.stderr.decode()}")
-        log.debug(f"{get_medialog(ele)} ffmpeg {r.stdout.decode()}")
-    else:
-        log.debug(f"{get_medialog(ele)} ffmpeg  decrypt success {newpath}")
-    pathlib.Path(item["path"]).unlink(missing_ok=True)
-    item["path"] = newpath
-    return item
+        if past_key:
+            key = past_key
+            log.debug(f"{get_medialog(ele)} got key from cache")
+        elif keymode == "manual":
+            key = await key_helper_manual(c, item["pssh"], ele.license, ele.id)
+        elif keymode == "keydb":
+            key = await key_helper_keydb(c, item["pssh"], ele.license, ele.id)
+        elif keymode == "cdrm":
+            key = await key_helper_cdrm(c, item["pssh"], ele.license, ele.id)
+        elif keymode == "cdrm2":
+            key = await key_helper_cdrm2(c, item["pssh"], ele.license, ele.id)
+        if not key:
+            raise Exception(f"{get_medialog(ele)} Could not get key")
+        await asyncio.get_event_loop().run_in_executor(
+            common_globals.thread,
+            partial(cache.set, ele.license, key, expire=constants.getattr("KEY_EXPIRY")),
+        )
+        log.debug(f"{get_medialog(ele)} got key")
+        newpath = pathlib.Path(
+            re.sub("\.part$", "", str(item["path"]), flags=re.IGNORECASE)
+        )
+        log.debug(
+            f"{get_medialog(ele)}  renaming {pathlib.Path(item['path']).absolute()} -> {newpath}"
+        )
+        ffmpeg_key = get_ffmpeg_key(key)
+        r = subprocess.run(
+            [
+                settings.get_ffmpeg(),
+                "-decryption_key",
+                ffmpeg_key,
+                "-i",
+                str(item["path"]),
+                "-codec",
+                "copy",
+                str(newpath),
+                "-y"
+            ],
+            stdout=subprocess.PIPE,
+            stderr=subprocess.PIPE,
+        )
+        if not pathlib.Path(newpath).exists():
+            log.debug(f"{get_medialog(ele)} ffmpeg decryption failed")
+            log.debug(f"{get_medialog(ele)} ffmpeg {r.stderr.decode()}")
+            log.debug(f"{get_medialog(ele)} ffmpeg {r.stdout.decode()}")
+        else:
+            log.debug(f"{get_medialog(ele)} ffmpeg  decrypt success {newpath}")
+        pathlib.Path(item["path"]).unlink(missing_ok=True)
+        item["path"] = newpath
+        return item
+    except Exception as E:
+        log.traceback_(E)
 
 
 def get_ffmpeg_key(key):
