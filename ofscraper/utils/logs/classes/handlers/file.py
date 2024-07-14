360165236d1caa0a3aa76298ac69870f5f0bf244
datawhores <datawhores@users.noreply.github.com>
2024-07-13 21:12:20 -0500

    don't log none in streamhandler

--- ofscraper/utils/logs/classes/handlers/file.py
+++ ofscraper/utils/logs/classes/handlers/file.py
@@ -1,7 +1,6 @@
 import logging
 import asyncio
 from filelock import FileLock
-from ofscraper.utils.system.system import getName
 import ofscraper.utils.paths.common as common_paths
 
 class StreamHandlerMulti(logging.StreamHandler):
@@ -11,17 +10,19 @@ class StreamHandlerMulti(logging.StreamHandler):
         self.lock=FileLock(common_paths.getRich())
         self.loop = asyncio.new_event_loop()
     def emit(self, record):
-        self.loop.create_task(self._async_emit(record))
+        if record.message=="None" or record.message=="":
+            return
+        super().emit(record)
 
-    async def _async_emit(self, record):
-        try:
-            super().emit(record)
-        except Exception as e:
-            print(e)
-    def close(self):
-        # with self.lock:
-        self.loop.run_until_complete(asyncio.gather(*asyncio.all_tasks(self.loop)))
-        self.loop.close()
+    # async def _async_emit(self, record):
+    #     try:
+    #         super().emit(record)
+    #     except Exception as e:
+    #         print(e)
+    # def close(self):
+    #     # with self.lock:
+    #     self.loop.run_until_complete(asyncio.gather(*asyncio.all_tasks(self.loop)))
+    #     self.loop.close()
 
         
 
