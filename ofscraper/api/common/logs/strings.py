2b052948d6df92222060bffb05c2bf887de8a1ab
datawhores <datawhores@users.noreply.github.com>
2024-07-14 19:10:11 -0500

    more changes to trace logs

--- ofscraper/api/common/logs/strings.py
+++ ofscraper/api/common/logs/strings.py
@@ -1,12 +1,12 @@
 
 
 
-PROGRESS_IDS = "\[[bold]{} In progress ids[/bold]\] "
-PROGRESS_RAW_TITLE= "\[[bold]{} In progress raw data[/bold]\] total item"
+PROGRESS_IDS = "[bold]\[{} In progress ids][/bold] "
+PROGRESS_RAW_TITLE= "[bold]\[{} In progress raw data][/bold] total item"
 
-PROGRESS_RAW = "\[[bold]{} In progress raw data[/bold]\] current item count: {} data: {}"
+PROGRESS_RAW = "[bold]\[{} In progress raw data][/bold] current item count: {} data: {}"
 
-PROGRESS_RAW_OFFSET= "\[[bold]{} In progress raw data[/bold]\] offset: {} current item count: {} data: {}"
+PROGRESS_RAW_OFFSET= "[bold]\[{} In progress raw data][/bold]]offset: {} current item count: {} data: {}"
 
 FINAL_IDS = "[bold]{} Final ids[/bold] "
 FINAL_RAW = "[bold]{} Final raw data[/bold]"
