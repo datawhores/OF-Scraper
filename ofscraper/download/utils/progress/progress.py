62f1ba00604930ca3a37db946fb72b79d534abbe
datawhores <datawhores@users.noreply.github.com>
2024-07-23 12:39:26 -0500

    use humanfriendly

--- ofscraper/download/utils/progress/progress.py
+++ ofscraper/download/utils/progress/progress.py
@@ -1,15 +1,19 @@
 import math
+from humanfriendly import format_size
 
 import ofscraper.download.utils.globals as common_globals
 
 
 def convert_num_bytes(num_bytes: int) -> str:
-    if num_bytes <=0:
-        return "0 B"
-    num_digits = int(math.log10(num_bytes)) + 1
-    if num_digits >= 10:
-        return f"{round(num_bytes / 10**9, 2)} GB"
-    return f"{round(num_bytes / 10 ** 6, 2)} MB"
+    try:
+        format_size(num_bytes)
+    except Exception:
+        if num_bytes <=0:
+            return "0 B"
+        num_digits = int(math.log10(num_bytes)) + 1
+        if num_digits >= 10:
+            return f"{round(num_bytes / 10**9, 2)} GB"
+        return f"{round(num_bytes / 10 ** 6, 2)} MB"
 
 
 async def update_total(update):
