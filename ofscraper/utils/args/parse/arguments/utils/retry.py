842d88178f985ff93ae7a45da7d3047b33eaecf8
datawhores <datawhores@users.noreply.github.com>
2024-08-28 15:33:47 -0500

    example wrapper

--- ofscraper/utils/args/parse/arguments/utils/retry.py
+++ ofscraper/utils/args/parse/arguments/utils/retry.py
@@ -6,3 +6,14 @@ def retry_modifiy(ctx):
         ctx.params["after"] = arrow.get(2000)
         ctx.params["no_api_cache"]=True
     return ctx
+
+# def check_mode_changes(func):
+#     @functools.wraps(func)
+#     # @click.pass_context
+#     def wrapper(ctx, *args, **kwargs):
+#         # fix before for check modes
+#         ctx.params["before"] = date_helper.before_callback(ctx, ctx.params, None)
+
+#         return func(ctx, *args, **kwargs)
+
+#     return wrapper
