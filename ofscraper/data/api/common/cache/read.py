401dc4d5e1e4d628924fe42f5ec10f437b807204
datawhores <datawhores@users.noreply.github.com>
2024-09-07 14:28:15 -0500

    change cache keys for our v2 of api

--- ofscraper/data/api/common/cache/read.py
+++ ofscraper/data/api/common/cache/read.py
@@ -5,16 +5,16 @@ import ofscraper.utils.cache as cache
 def read_full_after_scan_check(model_id, api):
     api = api.lower()
     return cache.get(
-        f"{model_id}_full_{api}_scrape",
+        f"{model_id}_full_{api}_v2_scrape",
         read_args.retriveArgs().after is not None,
     )
 
 
 def read_one_good_scan_check(model_id, api):
     api = api.lower()
-    return cache.get(f"{api}_one_good_scan_{model_id}")
+    return cache.get(f"{api}_v2_one_good_scan_{model_id}")
 
 
 def read_check_mode(model_id, api):
     api = api.lower()
-    return cache.get(f"{api}_check_{model_id}", default=[])
+    return cache.get(f"{api}_v2_check_{model_id}", default=[])
