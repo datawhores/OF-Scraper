45521858eb859c6ff1ea76bb831d9238cf6f2ba4
datawhores <datawhores@users.noreply.github.com>
2024-09-09 11:22:43 -0500

    remove bold yellow and for message

--- ofscraper/utils/logs/logs.py
+++ ofscraper/utils/logs/logs.py
@@ -79,7 +79,7 @@ def print_start_message():
             data=re.sub("\n","",j.text_())
             if not data:
                 return     
-            log.error(f"[bold yellow]{data}[/bold yellow]")
+            log.error(f"{data}")
 def print_latest_version():
     log = logging.getLogger("shared")
     with manager.Manager.get_session(backend="httpx") as  sess:
