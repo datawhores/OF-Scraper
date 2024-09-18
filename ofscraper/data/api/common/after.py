3cc4bd00f733af1f626bf1ebc5268e3522c89084
datawhores <datawhores@users.noreply.github.com>
2024-09-18 06:30:09 -0500

    change how get after works and change wording

--- ofscraper/data/api/common/after.py
+++ ofscraper/data/api/common/after.py
@@ -7,8 +7,8 @@ from ofscraper.data.api.common.cache.read import read_full_after_scan_check
 
 def get_after_pre_checks(model_id, api):
     val = None
-    if forced_after is not None:
-        val = forced_after
+    if read_args.retriveArgs().after==0:
+        val=0
     elif read_args.retriveArgs().after is not None:
         val = read_args.retriveArgs().after.float_timestamp
     elif not settings.auto_after_enabled():
@@ -20,8 +20,8 @@ def get_after_pre_checks(model_id, api):
 
 def _return_val(val, api):
     if val is None:
-        logging.getLogger("shared").info(f"precheck failed for {api} using db")
+        logging.getLogger("shared").info(f"precheck failed for setting after {api} using db")
         return val
     else:
-        logging.getLogger("shared").info(f"precheck success for {api} skipping db")
+        logging.getLogger("shared").info(f"precheck success for setting after {api} skipping db")
         return val
