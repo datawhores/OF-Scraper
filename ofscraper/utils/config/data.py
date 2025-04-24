5672a2896d7af1ebbafbb098819a1c37b63269bd
datawhores <datawhores@users.noreply.github.com>
2025-04-24 10:55:19 -0500

    deprechete disable autoafter

--- ofscraper/utils/config/data.py
+++ ofscraper/utils/config/data.py
@@ -232,12 +232,6 @@ def get_InfiniteLoop(config=None):
 def get_enable_after(config=None):
     if config is False:
         return constants.ENABLE_AUTO_AFTER_DEFAULT
-    val = not config.get("disable_auto_after")
-    val = (
-        not config.get("advanced_options", {}).get("disable_auto_after")
-        if val is None
-        else val
-    )
     val = config.get("enable_auto_after") if val is None else val
     val = (
         config.get("advanced_options", {}).get("enable_auto_after")
