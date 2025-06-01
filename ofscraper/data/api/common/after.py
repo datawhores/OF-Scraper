05d715ac83fdc673a5f1de75b7ad0109b29bf7cc
datawhores <datawhores@users.noreply.github.com>
2025-06-01 17:30:18 -0500

    refactor: improve logging for --after

--- ofscraper/data/api/common/after.py
+++ ofscraper/data/api/common/after.py
@@ -3,32 +3,20 @@ import logging
 import ofscraper.utils.settings as settings
 from ofscraper.data.api.common.cache.read import read_full_after_scan_check
 
-
+log= logging.getLogger("shared")
 def get_after_pre_checks(model_id, api):
-    val = None
-    if settings.get_settings().after == 0:
-        val = 0
-    elif settings.get_settings().after is not None:
-        val = settings.get_settings().after.float_timestamp
+    if settings.get_settings().after is not None:
+        log.debug(f"{api}: using provided value for after")
+        if settings.get_settings().after !=0:
+            return settings.get_settings().after.float_timestamp
+        else:
+            return 0
     elif not settings.get_settings().auto_after:
-        val = 0
+        log.debug(f"{api}: auto-after is disabled using 0")
+        return 0
     elif read_full_after_scan_check(model_id, api):
-        val = 0
-    elif "like" in settings.get_settings().action:
-        val = 0
-    elif "unlike" in settings.get_settings().action:
-        val = 0
-    return _return_val(val, api)
-
-
-def _return_val(val, api):
-    if val is None:
-        logging.getLogger("shared").info(
-            f"precheck failed for setting after {api} using db"
-        )
-        return val
-    else:
-        logging.getLogger("shared").info(
-            f"precheck success for setting after {api} skipping db"
-        )
-        return val
+        log.debug(f"{api}: full scan has been trigger")
+        return 0
+    elif settings.get_settings().action in {"like","unlike"}:
+        log.debug(f"{api}: doing full scan for action like/unlike")
+        return 0
