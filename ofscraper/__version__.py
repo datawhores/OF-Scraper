7eb5d2fa7efac24c2bcf79cb976fd2519d5b889e
datawhores <datawhores@users.noreply.github.com>
2025-06-08 16:50:41 -0500

    fix: revamp version

--- ofscraper/__version__.py
+++ ofscraper/__version__.py
@@ -1,34 +1,9 @@
-r"""
-        _____                                               
-  _____/ ____\______ ________________    ____   ___________ 
- /  _ \   __\/  ___// ___\_  __ \__  \  /  _ \_/ __ \_  __ \
-(  <_> )  |  \___ \\  \___|  | \// __ \(  <_> )  ___/|  | \/
- \____/|__| /____  >\___  >__|  (____  /\____/ \___  >__|   
-                 \/     \/           \/            \/       
-"""
-
-__title__ = "ofscraper"
-__author__ = "datawhores"
-__author_email__ = "datawhores@riseup.net"
-__description__ = (
-    "A command-line program to quickly download,like or unlike posts, and more"
-)
-__url__ = "https://github.com/datawhores/OF-Scraper"
-__license__ = "GNU General Public License v3 or later (GPLv3+)"
-__copyright__ = "Copyright 2023"
-
+import importlib.metadata
 
 try:
-    from dunamai import Version
-
-    __hardcoded__ = None
-    __version__ = __hardcoded__ or Version.from_git(
-        pattern="(?P<base>\d+\.\d+(\.((\d+\.\w+)|\w+)|))"
-    ).serialize(format="{base}+{branch}.{commit}", metadata=False)
-    # if __version__ == "0.0.0":
-    #     print(__version__)
-    # raise Exception("pipx error")
-except:
-    import importlib.metadata
-
+    # This will read the version from the installed package's metadata
     __version__ = importlib.metadata.version("ofscraper")
+except importlib.metadata.PackageNotFoundError:
+    # This is a fallback for when the package is not installed, e.g.,
+    # when running in a development environment without installation.
+    __version__ = "0.0.0"
