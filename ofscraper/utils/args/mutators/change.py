2d64e9af03935a0e928bfd641e33b361a1a2cdc4
datawhores <datawhores@users.noreply.github.com>
2025-06-30 07:42:26 -0500

    chore: delete unneeded files

--- ofscraper/utils/args/mutators/change.py
+++
@@ -1,8 +0,0 @@
-from ofscraper.utils.args.accessors.read import retriveArgs
-from ofscraper.utils.args.mutators.write import setArgs
-
-
-def changeargs(newargs):
-    args = retriveArgs()
-    args = setArgs(args.update(newargs))
-    return args
