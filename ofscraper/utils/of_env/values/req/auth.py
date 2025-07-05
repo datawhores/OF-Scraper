8fa871ebed797915a61e62f1c8be97fe8f3b6991
datawhores <datawhores@users.noreply.github.com>
2025-07-05 17:30:15 -0500

    chore: move activity env to new file

---
+++ ofscraper/utils/of_env/values/req/auth.py
@@ -0,0 +1,8 @@
+import os
+def load_misc_config():
+    config={} 
+    # AUTH_WARNING_TIMEOUT: Time to wait between printing auth warnings (seconds).)
+    config["AUTH_WARNING_TIMEOUT"] = float(
+        os.getenv("OFSC_AUTH_WARNING_TIMEOUT", "1800")
+    )
+    return config
