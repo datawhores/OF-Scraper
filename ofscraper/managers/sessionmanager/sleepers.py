9b9facf88a10ebe944b394264085628d65a749d3
datawhores <datawhores@users.noreply.github.com>
2025-07-20 18:02:59 -0500

    refactor: move all sleeper code to lazily loaded class

--- ofscraper/managers/sessionmanager/sleepers.py
+++ ofscraper/managers/sessionmanager/sleepers.py
@@ -1,156 +1,127 @@
 import ofscraper.utils.of_env.of_env as env
 from ofscraper.managers.sessionmanager.sessionmanager import SessionSleep
 
+class Sleeper:
+    """
+    Manages the creation of SessionSleep instances, creating them on-demand
+    to ensure they use the most current environment settings.
+    """
+    def __init__(self):
+        """
+        Initializes all sleeper instances to None. They will be created
+        on first access.
+        """
+        self._rate_limit = None
+        self._forbidden = None
+        self._download_rate_limit = None
+        self._download_forbidden = None
+        self._cdm_rate_limit = None
+        self._cdm_forbidden = None
+        self._like_rate_limit = None
+        self._like_forbidden = None
+        self._metadata_rate_limit = None
+        self._metadata_forbidden = None
+        self._discord_rate_limit = None
+        self._discord_forbidden = None
+        self._subscription_rate_limit = None
+        self._subscription_forbidden = None
 
-rate_limit_session_sleeper = SessionSleep(
-    sleep=env.getattr("SESSION_SLEEP_INIT"),
-    min_sleep=env.getattr("SESSION_MIN_SLEEP"),
-    difmin=env.getattr("SESSION_SLEEP_INCREASE_TIME_DIFF"),
-    max_sleep=env.getattr("SESSION_SLEEP_MAX"),
-    decay_threshold=env.getattr("SESSION_SLEEP_DECAY_THRESHOLD"),
-    decay_factor=env.getattr("SESSION_SLEEP_DECAY_FACTOR"),
-    increase_factor=env.getattr("SESSION_SLEEP_INCREASE_FACTOR"),
-    error_name="SESSION_RATE_LIMIT",
-)
+    def _create_sleeper(self, prefix: str, error_name: str) -> SessionSleep:
+        """A helper factory to create a SessionSleep instance."""
+        return SessionSleep(
+            sleep=env.getattr(f"{prefix}_SLEEP_INIT"),
+            min_sleep=env.getattr(f"{prefix}_MIN_SLEEP"),
+            difmin=env.getattr(f"{prefix}_SLEEP_INCREASE_TIME_DIFF"),
+            max_sleep=env.getattr(f"{prefix}_SLEEP_MAX"),
+            decay_threshold=env.getattr(f"{prefix}_DECAY_THRESHOLD"),
+            decay_factor=env.getattr(f"{prefix}_DECAY_FACTOR"),
+            increase_factor=env.getattr(f"{prefix}_SLEEP_INCREASE_FACTOR"),
+            error_name=error_name,
+        )
 
-forbidden_session_sleeper = SessionSleep(
-    sleep=env.getattr("SESSION_403_SLEEP_INIT"),
-    min_sleep=env.getattr("SESSION_403_MIN_SLEEP"),
-    difmin=env.getattr("SESSION_403_SLEEP_INCREASE_TIME_DIFF"),
-    max_sleep=env.getattr("SESSION_403_SLEEP_MAX"),
-    decay_threshold=env.getattr("SESSION_403_SLEEP_DECAY_THRESHOLD"),
-    decay_factor=env.getattr("SESSION_403_SLEEP_DECAY_FACTOR"),
-    increase_factor=env.getattr("SESSION_403_SLEEP_INCREASE_FACTOR"),
-    error_name="SESSION_403",
-)
+    @property
+    def rate_limit_session_sleeper(self) -> SessionSleep:
+        if self._rate_limit is None:
+            self._rate_limit = self._create_sleeper("SESSION", "SESSION_RATE_LIMIT")
+        return self._rate_limit
 
-download_rate_limit_session_sleeper = SessionSleep(
-    sleep=env.getattr("DOWNLOAD_SESSION_SLEEP_INIT"),
-    min_sleep=env.getattr("DOWNLOAD_SESSION_MIN_SLEEP"),
-    difmin=env.getattr("DOWNLOAD_SESSION_SLEEP_INCREASE_TIME_DIFF"),
-    max_sleep=env.getattr("DOWNLOAD_SESSION_SLEEP_MAX"),
-    decay_threshold=env.getattr("DOWNLOAD_SESSION_SLEEP_DECAY_THRESHOLD"),
-    decay_factor=env.getattr("DOWNLOAD_SESSION_SLEEP_DECAY_FACTOR"),
-    increase_factor=env.getattr("DOWNLOAD_SESSION_SLEEP_INCREASE_FACTOR"),
-    error_name="DOWNLOAD_SESSION_RATE_LIMIT",
-)
+    @property
+    def forbidden_session_sleeper(self) -> SessionSleep:
+        if self._forbidden is None:
+            self._forbidden = self._create_sleeper("SESSION_403", "SESSION_403")
+        return self._forbidden
 
-download_forbidden_session_sleeper = SessionSleep(
-    sleep=env.getattr("DOWNLOAD_SESSION_403_SLEEP_INIT"),
-    min_sleep=env.getattr("DOWNLOAD_SESSION_403_MIN_SLEEP"),
-    difmin=env.getattr("DOWNLOAD_SESSION_403_SLEEP_INCREASE_TIME_DIFF"),
-    max_sleep=env.getattr("DOWNLOAD_SESSION_403_SLEEP_MAX"),
-    decay_threshold=env.getattr("DOWNLOAD_SESSION_403_SLEEP_DECAY_THRESHOLD"),
-    decay_factor=env.getattr("DOWNLOAD_SESSION_403_SLEEP_DECAY_FACTOR"),
-    increase_factor=env.getattr("DOWNLOAD_SESSION_403_SLEEP_INCREASE_FACTOR"),
-    error_name="DOWNLOAD_SESSION_403",
-)
+    @property
+    def download_rate_limit_session_sleeper(self) -> SessionSleep:
+        if self._download_rate_limit is None:
+            self._download_rate_limit = self._create_sleeper("DOWNLOAD_SESSION", "DOWNLOAD_SESSION_RATE_LIMIT")
+        return self._download_rate_limit
 
-cdm_rate_limit_session_sleeper = SessionSleep(
-    sleep=env.getattr("CDM_SESSION_SLEEP_INIT"),
-    min_sleep=env.getattr("CDM_SESSION_MIN_SLEEP"),
-    difmin=env.getattr("CDM_SESSION_SLEEP_INCREASE_TIME_DIFF"),
-    max_sleep=env.getattr("CDM_SESSION_SLEEP_MAX"),
-    decay_threshold=env.getattr("CDM_SESSION_SLEEP_DECAY_THRESHOLD"),
-    decay_factor=env.getattr("CDM_SESSION_SLEEP_DECAY_FACTOR"),
-    increase_factor=env.getattr("CDM_SESSION_SLEEP_INCREASE_FACTOR"),
-    error_name="CDM_SESSION_RATE_LIMIT",
-)
+    @property
+    def download_forbidden_session_sleeper(self) -> SessionSleep:
+        if self._download_forbidden is None:
+            self._download_forbidden = self._create_sleeper("DOWNLOAD_SESSION_403", "DOWNLOAD_SESSION_403")
+        return self._download_forbidden
 
-cdm_forbidden_session_sleeper = SessionSleep(
-    sleep=env.getattr("CDM_SESSION_403_SLEEP_INIT"),
-    min_sleep=env.getattr("CDM_SESSION_403_MIN_SLEEP"),
-    difmin=env.getattr("CDM_SESSION_403_SLEEP_INCREASE_TIME_DIFF"),
-    max_sleep=env.getattr("CDM_SESSION_403_SLEEP_MAX"),
-    decay_threshold=env.getattr("CDM_SESSION_403_SLEEP_DECAY_THRESHOLD"),
-    decay_factor=env.getattr("CDM_SESSION_403_SLEEP_DECAY_FACTOR"),
-    increase_factor=env.getattr("CDM_SESSION_403_SLEEP_INCREASE_FACTOR"),
-    error_name="CDM_SESSION_403",
-)
+    @property
+    def cdm_rate_limit_session_sleeper(self) -> SessionSleep:
+        if self._cdm_rate_limit is None:
+            self._cdm_rate_limit = self._create_sleeper("CDM_SESSION", "CDM_SESSION_RATE_LIMIT")
+        return self._cdm_rate_limit
 
-like_rate_limit_session_sleeper = SessionSleep(
-    sleep=env.getattr("LIKE_SESSION_SLEEP_INIT"),
-    min_sleep=env.getattr("LIKE_SESSION_MIN_SLEEP"),
-    difmin=env.getattr("LIKE_SESSION_SLEEP_INCREASE_TIME_DIFF"),
-    max_sleep=env.getattr("LIKE_SESSION_SLEEP_MAX"),
-    decay_threshold=env.getattr("LIKE_SESSION_SLEEP_DECAY_THRESHOLD"),
-    decay_factor=env.getattr("LIKE_SESSION_SLEEP_DECAY_FACTOR"),
-    increase_factor=env.getattr("LIKE_SESSION_SLEEP_INCREASE_FACTOR"),
-    error_name="LIKE_SESSION_RATE_LIMIT",
-)
+    @property
+    def cdm_forbidden_session_sleeper(self) -> SessionSleep:
+        if self._cdm_forbidden is None:
+            self._cdm_forbidden = self._create_sleeper("CDM_SESSION_403", "CDM_SESSION_403")
+        return self._cdm_forbidden
 
-like_forbidden_session_sleeper = SessionSleep(
-    sleep=env.getattr("LIKE_SESSION_403_SLEEP_INIT"),
-    min_sleep=env.getattr("LIKE_SESSION_403_MIN_SLEEP"),
-    difmin=env.getattr("LIKE_SESSION_403_SLEEP_INCREASE_TIME_DIFF"),
-    max_sleep=env.getattr("LIKE_SESSION_403_SLEEP_MAX"),
-    decay_threshold=env.getattr("LIKE_SESSION_403_SLEEP_DECAY_THRESHOLD"),
-    decay_factor=env.getattr("LIKE_SESSION_403_SLEEP_DECAY_FACTOR"),
-    increase_factor=env.getattr("LIKE_SESSION_403_SLEEP_INCREASE_FACTOR"),
-    error_name="LIKE_SESSION_403",
-)
+    @property
+    def like_rate_limit_session_sleeper(self) -> SessionSleep:
+        if self._like_rate_limit is None:
+            self._like_rate_limit = self._create_sleeper("LIKE_SESSION", "LIKE_SESSION_RATE_LIMIT")
+        return self._like_rate_limit
 
-metadata_rate_limit_session_sleeper = SessionSleep(
-    sleep=env.getattr("METADATA_SESSION_SLEEP_INIT"),
-    min_sleep=env.getattr("METADATA_SESSION_MIN_SLEEP"),
-    difmin=env.getattr("METADATA_SESSION_SLEEP_INCREASE_TIME_DIFF"),
-    max_sleep=env.getattr("METADATA_SESSION_SLEEP_MAX"),
-    decay_threshold=env.getattr("METADATA_SESSION_SLEEP_DECAY_THRESHOLD"),
-    decay_factor=env.getattr("METADATA_SESSION_SLEEP_DECAY_FACTOR"),
-    increase_factor=env.getattr("METADATA_SESSION_SLEEP_INCREASE_FACTOR"),
-    error_name="METADATA_SESSION_RATE_LIMIT",
-)
+    @property
+    def like_forbidden_session_sleeper(self) -> SessionSleep:
+        if self._like_forbidden is None:
+            self._like_forbidden = self._create_sleeper("LIKE_SESSION_403", "LIKE_SESSION_403")
+        return self._like_forbidden
 
-metadata_forbidden_session_sleeper = SessionSleep(
-    sleep=env.getattr("METADATA_SESSION_403_SLEEP_INIT"),
-    min_sleep=env.getattr("METADATA_SESSION_403_MIN_SLEEP"),
-    difmin=env.getattr("METADATA_SESSION_403_SLEEP_INCREASE_TIME_DIFF"),
-    max_sleep=env.getattr("METADATA_SESSION_403_SLEEP_MAX"),
-    decay_threshold=env.getattr("METADATA_SESSION_403_SLEEP_DECAY_THRESHOLD"),
-    decay_factor=env.getattr("METADATA_SESSION_403_SLEEP_DECAY_FACTOR"),
-    increase_factor=env.getattr("METADATA_SESSION_403_SLEEP_INCREASE_FACTOR"),
-    error_name="METADATA_SESSION_403",
-)
-discord_rate_limit_session_sleeper = SessionSleep(
-    sleep=env.getattr("DISCORD_SESSION_SLEEP_INIT"),
-    min_sleep=env.getattr("DISCORD_SESSION_MIN_SLEEP"),
-    difmin=env.getattr("DISCORD_SESSION_SLEEP_INCREASE_TIME_DIFF"),
-    max_sleep=env.getattr("DISCORD_SESSION_SLEEP_MAX"),
-    decay_threshold=env.getattr("DISCORD_SESSION_SLEEP_DECAY_THRESHOLD"),
-    decay_factor=env.getattr("DISCORD_SESSION_SLEEP_DECAY_FACTOR"),
-    increase_factor=env.getattr("DISCORD_SESSION_SLEEP_INCREASE_FACTOR"),
-    error_name="DISCORD_SESSION_RATE_LIMIT",
-)
+    @property
+    def metadata_rate_limit_session_sleeper(self) -> SessionSleep:
+        if self._metadata_rate_limit is None:
+            self._metadata_rate_limit = self._create_sleeper("METADATA_SESSION", "METADATA_SESSION_RATE_LIMIT")
+        return self._metadata_rate_limit
 
-discord_forbidden_session_sleeper = SessionSleep(
-    sleep=env.getattr("DISCORD_SESSION_403_SLEEP_INIT"),
-    min_sleep=env.getattr("DISCORD_SESSION_403_MIN_SLEEP"),
-    difmin=env.getattr("DISCORD_SESSION_403_SLEEP_INCREASE_TIME_DIFF"),
-    max_sleep=env.getattr("DISCORD_SESSION_403_SLEEP_MAX"),
-    decay_threshold=env.getattr("DISCORD_SESSION_403_SLEEP_DECAY_THRESHOLD"),
-    decay_factor=env.getattr("DISCORD_SESSION_403_SLEEP_DECAY_FACTOR"),
-    increase_factor=env.getattr("DISCORD_SESSION_403_SLEEP_INCREASE_FACTOR"),
-    error_name="DISCORD_SESSION_403",
-)
+    @property
+    def metadata_forbidden_session_sleeper(self) -> SessionSleep:
+        if self._metadata_forbidden is None:
+            self._metadata_forbidden = self._create_sleeper("METADATA_SESSION_403", "METADATA_SESSION_403")
+        return self._metadata_forbidden
 
-subscription_rate_limit_session_sleeper = SessionSleep(
-    sleep=env.getattr("SUBSCRIPTION_SESSION_SLEEP_INIT"),
-    min_sleep=env.getattr("SUBSCRIPTION_SESSION_MIN_SLEEP"),
-    difmin=env.getattr("SUBSCRIPTION_SESSION_SLEEP_INCREASE_TIME_DIFF"),
-    max_sleep=env.getattr("SUBSCRIPTION_SESSION_SLEEP_MAX"),
-    decay_threshold=env.getattr("SUBSCRIPTION_SESSION_SLEEP_DECAY_THRESHOLD"),
-    decay_factor=env.getattr("SUBSCRIPTION_SESSION_SLEEP_DECAY_FACTOR"),
-    increase_factor=env.getattr("SUBSCRIPTION_SESSION_SLEEP_INCREASE_FACTOR"),
-    error_name="SUBSCRIPTION_SESSION_RATE_LIMIT",
-)
+    @property
+    def discord_rate_limit_session_sleeper(self) -> SessionSleep:
+        if self._discord_rate_limit is None:
+            self._discord_rate_limit = self._create_sleeper("DISCORD_SESSION", "DISCORD_SESSION_RATE_LIMIT")
+        return self._discord_rate_limit
 
-subscription_forbidden_session_sleeper = SessionSleep(
-    sleep=env.getattr("SUBSCRIPTION_SESSION_403_SLEEP_INIT"),
-    min_sleep=env.getattr("SUBSCRIPTION_SESSION_403_MIN_SLEEP"),
-    difmin=env.getattr("SUBSCRIPTION_SESSION_403_SLEEP_INCREASE_TIME_DIFF"),
-    max_sleep=env.getattr("SUBSCRIPTION_SESSION_403_SLEEP_MAX"),
-    decay_threshold=env.getattr("SUBSCRIPTION_SESSION_403_SLEEP_DECAY_THRESHOLD"),
-    decay_factor=env.getattr("SUBSCRIPTION_SESSION_403_SLEEP_DECAY_FACTOR"),
-    increase_factor=env.getattr("SUBSCRIPTION_SESSION_403_SLEEP_INCREASE_FACTOR"),
-    error_name="SUBSCRIPTION_SESSION_403",
-)
+    @property
+    def discord_forbidden_session_sleeper(self) -> SessionSleep:
+        if self._discord_forbidden is None:
+            self._discord_forbidden = self._create_sleeper("DISCORD_SESSION_403", "DISCORD_SESSION_403")
+        return self._discord_forbidden
+
+    @property
+    def subscription_rate_limit_session_sleeper(self) -> SessionSleep:
+        if self._subscription_rate_limit is None:
+            self._subscription_rate_limit = self._create_sleeper("SUBSCRIPTION_SESSION", "SUBSCRIPTION_SESSION_RATE_LIMIT")
+        return self._subscription_rate_limit
+
+    @property
+    def subscription_forbidden_session_sleeper(self) -> SessionSleep:
+        if self._subscription_forbidden is None:
+            self._subscription_forbidden = self._create_sleeper("SUBSCRIPTION_SESSION_403", "SUBSCRIPTION_SESSION_403")
+        return self._subscription_forbidden
+
+# Create a single, global instance of the manager that can be imported elsewhere
+sleepers = Sleeper()
