a9a9eee234517c3ee8ca05782c6e53376ed56f37
datawhores <datawhores@users.noreply.github.com>
2025-10-16 11:03:43 -0500

    fix: update urls

--- ofscraper/utils/of_env/values/url/url.py
+++ ofscraper/utils/of_env/values/url/url.py
@@ -181,14 +181,14 @@ def load_api_endpoints_config():
     # Default: "https://onlyfans.com/api2/v2/posts/paid?limit=100&skip_users=all&format=infinite&offset={}&user_id={}"
     config["purchased_contentEP"] = os.getenv(
         "OFSC_PURCHASED_CONTENT_EP",
-        "https://onlyfans.com/api2/v2/posts/paid?limit=100&skip_users=all&format=infinite&offset={}&user_id={}",
+        "https://onlyfans.com/api2/v2/posts/paid/all?limit=100&skip_users=all&format=infinite&offset={}&author={}"
     )
 
     # purchased_contentALL: Endpoint for all purchased content.
     # Default: "https://onlyfans.com/api2/v2/posts/paid?limit=100&skip_users=all&format=infinite&offset={}"
     config["purchased_contentALL"] = os.getenv(
         "OFSC_PURCHASED_CONTENT_ALL_EP",
-        "https://onlyfans.com/api2/v2/posts/paid?limit=100&skip_users=all&format=infinite&offset={}",
+        "https://onlyfans.com/api2/v2/posts/paid/all?limit=100&skip_users=all&format=infinite&offset={}",
     )
 
     # highlightSPECIFIC: Endpoint for a specific highlight story.
