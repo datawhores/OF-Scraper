e4ccb9c7f49151a2806f85d5ae4eaf76f607ebdd
datawhores <datawhores@users.noreply.github.com>
2026-02-25 07:37:06 -0600

    refactor: add number to log

--- ofscraper/filters/media/filters.py
+++ ofscraper/filters/media/filters.py
@@ -175,7 +175,7 @@ def previous_download_filter(medialist, username=None, model_id=None):
     else:
         log.info("Downloading unique media across all models")
         media_ids = set(get_media_ids_downloaded(model_id=model_id, username=username))
-        log.debug("Number of unique media ids in database for all models")
+        log.debug(f"Number of unique media ids in database for all models: {len(media_ids)}")
         medialist = seperate.separate_by_id(medialist, media_ids)
         log.debug(f"Number of new mediaids with dupe ids removed: {len(medialist)}")
         medialist = seperate.seperate_avatars(medialist)
