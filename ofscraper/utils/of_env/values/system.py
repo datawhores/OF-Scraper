96a52a4941a3a7cea18032bd0253c7d3e8f17869
datawhores <datawhores@users.noreply.github.com>
2026-03-07 14:34:45 -0600

    Refactor: Change default subprocess log levels to DEBUG to prevent stdout console spam

--- ofscraper/utils/of_env/values/system.py
+++ ofscraper/utils/of_env/values/system.py
@@ -14,22 +14,22 @@ def load_system_config():
     ## LOG LEVEL of 0 will disable that output
     # Set log level for user-defined scripts
     config["AFTER_DOWNLOAD_ACTION_SCRIPT_SUBPROCESS_LEVEL"] = int(
-        os.getenv("OFSC_AFTER_DOWNLOAD_ACTION_SCRIPT_SUBPROCESS_LEVEL", "20")
+        os.getenv("OFSC_AFTER_DOWNLOAD_ACTION_SCRIPT_SUBPROCESS_LEVEL", "10")
     )
     config["AFTER_LIKE_ACTION_SCRIPT_SUBPROCESS_LEVEL"] = int(
-        os.getenv("OFSC_AFTER_LIKE_ACTION_SCRIPT_SUBPROCESS_LEVEL", "20")
+        os.getenv("OFSC_AFTER_LIKE_ACTION_SCRIPT_SUBPROCESS_LEVEL", "10")
     )
     config["NAMING_SCRIPT_SUBPROCESS_LEVEL"] = int(
-        os.getenv("OFSC_NAMING_SCRIPT_SUBPROCESS_LEVEL", "20")
+        os.getenv("OFSC_NAMING_SCRIPT_SUBPROCESS_LEVEL", "10")
     )
     config["SKIP_DOWNLOAD_SCRIPT_SUBPROCESS_LEVEL"] = int(
-        os.getenv("OFSC_SKIP_DOWNLOAD_SCRIPT_SUBPROCESS_LEVEL", "20")
+        os.getenv("OFSC_SKIP_DOWNLOAD_SCRIPT_SUBPROCESS_LEVEL", "10")
     )
     config["AFTER_DOWNLOAD_SCRIPT_SUBPROCESS_LEVEL"] = int(
-        os.getenv("OFSC_AFTER_DOWNLOAD_SCRIPT_SUBPROCESS_LEVEL", "20")
+        os.getenv("OFSC_AFTER_DOWNLOAD_SCRIPT_SUBPROCESS_LEVEL", "10")
     )
     config["FINAL_SCRIPT_SUBPROCESS_LEVEL"] = int(
-        os.getenv("OFSC_FINAL_SCRIPT_SUBPROCESS_LEVEL", "20")
+        os.getenv("OFSC_FINAL_SCRIPT_SUBPROCESS_LEVEL", "10")
     )
 
     config["LOG_SUBPROCESS_LEVEL"] = int(os.getenv("OFSC_LOG_SUBPROCESS_LEVELL", "10"))
