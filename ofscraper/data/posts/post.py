fd9d0a0fdbf38b25288ad8b5c57bbec06d0baffc
datawhores <datawhores@users.noreply.github.com>
2026-03-19 17:52:58 -0500

    chore: change wording

--- ofscraper/data/posts/post.py
+++ ofscraper/data/posts/post.py
@@ -593,7 +593,7 @@ async def process_tasks(model_id, username, ele, c=None):
                 )
         else:
             log.info(
-                f"Skipping wall APIs for {username} (Account is inactive and past grace period)"
+                f"Skipping wall/story APIs for {username} (Account is inactive and past grace period)"
             )
 
         # Gather results and add to postcollection
